4038c490d5ef4da641ddb0df3f50ede5a13461ae
Carlos Cuxim <carlos.cuxim@gmail.com>
2022-04-16 01:42:50 -0500

    Agregado cajas de alerta en la misión 1

--- scripts/Mission1.js
+++ scripts/Mission1.js
@@ -5,17 +5,47 @@ function SendAnswer(){
     Answer = Answer.replace(/\s\s+/g, ' ')
     
     if(Answer == "ZHRHÑ SHÑ DTFÑQPTÑ RTWTF ÑTL EOYLQRHÑ") {
-        alert("CORRECTO")
+        Swal.fire({
+            icon: 'success',
+            title: '¡CORRECTO!',
+            text: 'El código de la misión es: ABCD',
+            footer: '<a href="./Experiment1.html">Experimentar</a>'
+          })
     } else {
-        alert("INCORRECTO")
+        Swal.fire({
+            icon: 'error',
+            title: '¡INCORRECTO!',
+            text: 'Intente de nuevo'
+          })
     }
 }
 
 
 function AnswerAlert(){
-    alert("La respuesta es: ZHRHÑ SHÑ DTFÑQPTÑ RTWTF ÑTL EOYLQRHÑ")
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "Recomiendo intentar el problema primero",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            html: 'La respuesta es:</br>ZHRHÑ SHÑ DTFÑQPTÑ RTWTF ÑTL EOYLQRHÑ',
+            icon: 'warning'
+          })
+        }
+      })
 }
 
 function HelpAlert(){
-    alert("Lo ideal es que aquí se coloque el video de ayuda")
+    Swal.fire({
+        html: `<iframe width="560" height="315" src="https://www.youtube.com/embed/sSL04hd3TDU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <center>VIDEO DE PRUEBA</center>`,
+        width: 700,
+        icon: "info"
+    })
 }
