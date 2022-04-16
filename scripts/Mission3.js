4ffff61113aa365fa46fa74d141e6fb644b3caeb
Carlos Cuxim <carlos.cuxim@gmail.com>
2022-04-16 02:05:30 -0500

    Agregado cajas de alerta para la misión 3

--- scripts/Mission3.js
+++ scripts/Mission3.js
@@ -5,17 +5,48 @@ function SendAnswer(){
     Answer = Answer.replace(/\s\s+/g, ' ')
     
     if(Answer == "2022") {
-        alert("CORRECTO")
+        Swal.fire({
+            icon: 'success',
+            title: '¡CORRECTO!',
+            text: 'El código de la misión es: WXYZ',
+            footer: '<a href="./Experiment3.html">Experimentar</a>'
+          })
     } else {
-        alert("INCORRECTO")
+        Swal.fire({
+            icon: 'error',
+            title: '¡INCORRECTO!',
+            text: 'Intente de nuevo'
+          })
     }
 }
 
 
 function AnswerAlert(){
-    alert("La respuesta es: 2022")
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "Recomiendo intentar el problema primero",
+        icon: 'warning',
+        showCancelButton: true,
+        //confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#c11d24',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            html: 'La respuesta es:</br>2020',
+            icon: 'warning'
+          })
+        }
+      })
 }
 
 function HelpAlert(){
-    alert("Lo ideal es que aquí se coloque el video de ayuda")
+    Swal.fire({
+        html: `<iframe width="560" height="315" src="https://www.youtube.com/embed/f9b0wwhTmeU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <center>VIDEO DE PRUEBA</center>`,
+        width: 700,
+        icon: "info"
+    })
 }
+ 
