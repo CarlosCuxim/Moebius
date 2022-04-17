69193846f20ad353ea973f41991480e4846be99f
Carlos Cuxim <carlos.cuxim@gmail.com>
2022-04-17 18:46:55 -0500

    Agregado botón de descifrar en el experimento 1

--- scripts/Experiment1.js
+++ scripts/Experiment1.js
@@ -1,28 +1,41 @@
 const Alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 
+
+/* FUNCIONES AUXILIARES */
 function CleanMessage(message) {
     message = message.trim()
     message = message.toUpperCase()
     message = message.replace(/\s\s+/g, ' ')
-    message = message.replace(/Á/g, 'A')
-    message = message.replace(/É/g, 'E')
-    message = message.replace(/Í/g, 'I')
-    message = message.replace(/Ó/g, 'O')
-    message = message.replace(/Ú/g, 'U')
-    message = message.replace(/Ü/g, 'U')
+    message = message.replace(/[ÁÄ]/g, 'A')
+    message = message.replace(/[ÉË]/g, 'E')
+    message = message.replace(/[ÍÏ]/g, 'I')
+    message = message.replace(/[ÓÖ]/g, 'O')
+    message = message.replace(/[ÚÜ]/g, 'U')
 
     return message
 }
 
-function Cipher(Message){
 
+/* FUNCIONES PARA EL CIFRADO */
+
+function GetLetterAssignment(inverse=false){
     let LetterAssignment = {}
 
     for(let i=0; i<Alphabet.length; i++){
         let L = Alphabet[i]
+        let P = document.getElementById(L).value
 
-        LetterAssignment[L] = document.getElementById(L).value
+        if(inverse){
+            LetterAssignment[P] = L
+        } else {
+            LetterAssignment[L] = P
+        }
     }
+
+    return LetterAssignment
+}
+
+function Cipher(Message, LetterAssignment){
 
     Message = CleanMessage(Message)
 
@@ -40,10 +53,16 @@ function Cipher(Message){
     return CypheredMessage
 }
 
+
+
+
 // FUNCIONES DE LOS BOTONES
 function SendAnswer(){
     const Message = document.getElementById("Message").value
     let Answer = document.getElementById("Answer")
 
-    Answer.value = Cipher(Message)
+    let INVERSE = document.getElementById("INVERSE").checked
+
+    let LetterAssignment = GetLetterAssignment(INVERSE)
+    Answer.value = Cipher(Message, LetterAssignment)
 }
